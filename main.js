4fe681118ed02999cdd1e733b67981ea83a11041
Sam McGrath <mcg13004@byui.edu>
2018-01-30 11:06:38 -0700

    got XML read in correctly. now need to get assignments and replace html_url

--- main.js
+++ main.js
@@ -25,8 +25,11 @@ var arr = [];
 module.exports = (course, stepCallback) => {
     course.message('repair-quicklinks child module launched.');
 
+    //this function goes through the course and retrieves then traverses through all of the files
     function retrieveFiles(course, functionCallback) {
+        getDropboxName();
         canvas.get(`/api/v1/courses/${course.info.canvasOU}/pages`, (err, pages) => {
+            //begin process of parsing
             htmlChecker(course, pages, (htmlCheckerErr) => {
                 if (htmlCheckerErr) {
                     functionCallback(htmlCheckerErr);
@@ -38,8 +41,8 @@ module.exports = (course, stepCallback) => {
         });
     }
 
+    //this function goes throug hte html and extracts all of the dropbox links.
     function htmlChecker(course, pages, functionCallback) {
-        // console.log(`LENGTH: ${pages.length}`)
         asyncLib.eachSeries(pages, (page, eachSeriesCallback) => {
             //p is received as a one-element array consisting of a Page object
             canvas.get(`/api/v1/courses/${course.info.canvasOU}/pages/${page.url}`, (err, p) => {
@@ -47,21 +50,20 @@ module.exports = (course, stepCallback) => {
                     eachSeriesCallback(err);
                     return;
                 } else {
-                    // console.log(JSON.stringify(p[0].body));
-                    // console.log(`PAGE TITLE: ${p[0].title}`)
+                    //load the html and grab all of the links
                     var $ = cheerio.load(p[0].body);
                     links = $('a');
                     $(links).each((i, link) => {
-                        // console.log(`LINK ATTR: ${$(link).attr('href')}`);
+                        console.log(`Link: ${JSON.stringify($(link).attr('href'))}`);
+                        //all dropbox links contain the word 'drop_box'
                         if ($(link).attr('href').indexOf('drop_box') != -1) {
-                            console.log(`LINK: ${$(link).attr('href')}`);
+                            console.log(`Found dropbox link`);
                             repairDropbox(course, p[0], (err) => {
                                 if (err) {
                                     eachSeries(err);
                                 }
                             });
                         }
-                        // console.log(`Link: ${$(link).text()} ${$(link).attr('href')}`);
                     });
                     eachSeriesCallback(null, course);
                 }
@@ -75,19 +77,79 @@ module.exports = (course, stepCallback) => {
     }
 
     function repairDropbox(course, page, functionCallback) {
-        // console.log(`Dropbox function reached: ${page.title}`);
         canvas.get(`/api/v1/courses/${course.info.canvasOU}/assignments`, (err, assignments) => {
             asyncLib.eachSeries(assignments, (assign, eachSeriesCallback) => {
+                //all dropboxes have the submission type of 'online_upload'
+                console.log(JSON.stringify(assign));
                 if (assign.submission_types.includes('online_upload')) {
-                    console.log(`TITLE: ${assign.name}`);
+                    //call lambda function to retrive html url
+                    arr.map((item) => {
+                        getDropboxes(course, item.name, (err) => {
+                            if (err) {
+                                eachSeriesCallback(err);
+                            } else {
+                                eachSeriesCallback(null);
+                            }
+                        });
+                    });
                 }
             }, (err) => {
                 if (err) {
                     functionCallback(err);
                     return;
+                } else {
+                    functionCallback(null, course);
+                    return;
                 }
             });
         });
+    }
+
+    //5. Make API call to Canvas w/ search term of assignment name
+    //still needs work
+    function getDropboxes(course, title, functionCallback) {
+        canvas.get(`/api/v1/courses/${course.info.canvasOU}/assignments`, (err, dropboxes) => {
+            asyncLib.each(dropboxes, (item, eachLimitCallback) => {
+                if (err) {
+                    eachLimitCallback(err);
+                } else {
+                    if (item.name === title) {
+                        return item.html_url;
+                    }
+                }
+            });
+        }, (err) => {
+            if (err) {
+                functionCallback(err);
+                return;
+            } else {
+                functionCallback(null);
+                return;
+            }
+        });
+    }
+
+    //retrieve the dropbox names and store the results in the array
+    function getDropboxName() {
+        var dropbox = getDropboxFile();
+        var $ = dropbox.dom;
+
+        $('dropbox > folder').each((index, folder) => {
+            var obj = {
+                name: folder.attribs.name,
+                id: folder.attribs.id
+            };
+            arr.push(obj);
+        });
+    }
+
+    //parse through the course.info file to get the .xml which contains the name/id of the dropboxes
+    function getDropboxFile() {
+        var file = course.content.find((file) => {
+            return file.name === `dropbox_d2l.xml`;
+        });
+
+        return file;
     }
 
     function initiateWaterfall(course, functionCallback) {
@@ -108,8 +170,8 @@ module.exports = (course, stepCallback) => {
 
 
     /************************************************************************************
-    *                                     START HERE                                    *
-    ************************************************************************************/
+     *                                     START HERE                                    *
+     ************************************************************************************/
     initiateWaterfall(course, (waterfallErr, results) => {
         if (waterfallErr) {
             course.error(waterfallErr);
