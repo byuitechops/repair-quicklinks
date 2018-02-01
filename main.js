9918f869a20ed439e8a712d1eae6c58a4f18ae20
Sam McGrath <mcg13004@byui.edu>
2018-02-01 10:22:18 -0700

    replaced global variable with YUI module pattern. added documentation

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 /*eslint-env node, es6*/
 
-/* Module Description */
+/* Module Description:
+This module goes through all of the quicklinks and checks to see if the quicklinks to the quizzes 
+or dropboxes are broken. If they are broken, the child module goes through them and repairs the link
+*/
 
-/* Put dependencies here */
-
-/* Include this line only if you are going to use Canvas API */
 const canvas = require('canvas-wrapper');
 const asyncLib = require('async');
 const cheerio = require('cheerio');
 
-/* Variables */
-var arr = [];
+//utilizing YUI module pattern
+//https://stackoverflow.com/a/2613647/5646003
+var get_array = (() => {
+    var arr = [];
+
+    return {
+        push: (arg) => {
+            arr.push(arg);
+        },
+
+        get_length: () => {
+            return arr.length;
+        },
+
+        get: () => {
+            return arr;
+        }
+    };
+})();
 
 /* View available course object functions */
 // https://github.com/byuitechops/d2l-to-canvas-conversion-tool/blob/master/documentation/classFunctions.md
 
+//PROCESS:
 //1. Get HTML from Pages, Quizzes, Assignments and Discussion Boards
 //2. Sort through & find bad quicklinks
 //3. Get ID from bad link (drop_box_144) ==> you want 144
@@ -22,6 +40,7 @@ var arr = [];
 //5. Make API call to Canvas w/ search term of assignment name
 //6. Use returned assignment's ID to build new link
 //7. Update HTML w/ new link
+
 module.exports = (course, stepCallback) => {
     course.message('repair-quicklinks child module launched.');
 
@@ -57,9 +76,12 @@ module.exports = (course, stepCallback) => {
                     //load the html and grab all of the links
                     var $ = cheerio.load(p[0].body);
                     links = $('a');
+
+                    //iterate through the links.
                     $(links).each((i, link) => {
                         //all dropbox links contain the word 'drop_box'
                         if ($(link).attr('href').indexOf('drop_box') != -1) {
+                            //we found a dropbox quicklink. 
                             fixDropbox(course, p[0], (err) => {
                                 if (err) {
                                     eachSeriesCallback(err);
@@ -78,6 +100,15 @@ module.exports = (course, stepCallback) => {
         });
     }
 
+    /**************************************************
+    * fixDropbox
+    *
+    * This function goes through the link and retrieves
+    * the name/id from the array (which is built through the 
+    * dropbox_d2l.xml). This function then makes an api
+    * call to retrieve correct url and replace the 
+    * link. 
+    **************************************************/
     function fixDropbox(course, page, functionCallback) {
         var newUrl = '';
 
@@ -88,15 +119,12 @@ module.exports = (course, stepCallback) => {
         $(links).each((i, link) => {
             var url = $(link).attr('href');
 
-            // console.log(`Link: ${$(link).attr('href')}`);
-
             if (url.indexOf('drop_box') != -1) {
                 //get dropbox number
                 srcUrl = url.split('drop_box_').pop();
                 
                 //get names of dropbox for search_term part of assignments api.
-                arr.forEach((obj) => {
-                    // console.log(`obj.id = ${obj.id}. srcUrl = ${srcUrl}`);
+                get_array.get.forEach((obj) => {
                     if (obj.id === srcUrl) {
                         //search term part of the api allows us to retrieve the list of assignments that has that name. 
                         //here, we extract the html_url from this and return it so it can be properly embedded in the html.
@@ -107,6 +135,8 @@ module.exports = (course, stepCallback) => {
                             } else {
                                 //some assignments have similar names so more than one quizzes gets returned
                                 //this takes care of that situation
+                                //TODO: ensure that the newUrl is correct 
+                                //TODO: call replaceLink() to replace the bad link with the good link in the HTML using cheerio.
                                 if (info.length > 1) {
                                     course.message('There are more than one quizzes with a similar name');
                                     info.forEach((quiz) => {
@@ -132,6 +162,12 @@ module.exports = (course, stepCallback) => {
         });
     }
 
+    //TODO: fill this function to replace the link after the new url has been found.
+    function replaceLink() {
+
+    }
+
+    //not being used at the moment.
     function retrieveLink(name, functionCallback) {
         console.log(`Name: ${name}`);
 
@@ -139,13 +175,14 @@ module.exports = (course, stepCallback) => {
         
     }
 
+    //not being used at the moment.
     function repairDropbox(course, page, functionCallback) {
         canvas.get(`/api/v1/courses/${course.info.canvasOU}/assignments`, (err, assignments) => {
             asyncLib.eachSeries(assignments, (assign, eachSeriesCallback) => {
                 //all dropboxes have the submission type of 'online_upload'
                 if (assign.submission_types.includes('online_upload')) {
                     //call lambda function to retrive html url
-                    arr.map((item) => {
+                    get_array.get.map((item) => {
                         getDropboxes(course, item.name, (err) => {
                             if (err) {
                                 eachSeriesCallback(err);
@@ -168,7 +205,7 @@ module.exports = (course, stepCallback) => {
     }
 
     //5. Make API call to Canvas w/ search term of assignment name
-    //still needs work
+    //not being used at the moment.
     function getDropboxes(course, title, functionCallback) {
         canvas.get(`/api/v1/courses/${course.info.canvasOU}/assignments`, (err, dropboxes) => {
             asyncLib.each(dropboxes, (item, eachLimitCallback) => {
@@ -193,16 +230,19 @@ module.exports = (course, stepCallback) => {
 
     //retrieve the dropbox names and store the results in the array
     function getDropboxName() {
+        //retrieve the dropbox_d2l.xml file
         var dropbox = getDropboxFile();
         var $ = dropbox.dom;
 
+        //iterate through the xml nodes and retrieve the id and name 
+        //of each dropbox folder
         $('dropbox > folder').each((index, folder) => {
             var obj = {
                 name: folder.attribs.name,
                 id: folder.attribs.id
             };
             
-            arr.push(obj);
+            get_array.push(obj);
         });
     }
 
